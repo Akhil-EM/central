12e082973f9535bee0416e95e5ad01d35beb61fb
Akhil-EM <akhilemcalicut@gmail.com>
2021-06-22 20:10:32 +0530

    old commit reverte2

--- server.js
+++ server.js
@@ -9,9 +9,13 @@ const publicPath = path.join(__dirname, '..', 'public');
 
 
 app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(publicPath, 'index.html'));
+//  });
+
+app.get('/',(req,res)=>{
+   res.json({status:'success'});
+})
 
 app.listen(port, () =>{
    console.log(port)
