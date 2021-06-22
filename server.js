090727a6d4397c9be885c2fe38a67d831e8a6b0a
Akhil-EM <akhilemcalicut@gmail.com>
2021-06-22 20:19:48 +0530

    old commit reverte2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 // const path = require('path');
 // const express = require('express');
 // const app = express();
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 // const publicPath = path.join(__dirname, '..', 'public');
 
 
