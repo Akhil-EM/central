e8e1a8b52f864d7869b47ba4131e885a9c8c4718
Akhil-EM <akhilemcalicut@gmail.com>
2021-06-22 20:01:50 +0530

    old commit reverte2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
 // process.env.PUBLIC_URL+'/img/android-chrome-192x192.png'
 import Router from './router/Router'
-<<<<<<< HEAD
-=======
-import ProductCard from './components/ProductCard';
->>>>>>> parent of 18e0e3f (new meta tag added)
 
 function App() {
   return (
