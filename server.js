3700f6c0a7892ca319473b52c77f57f7fb801800
Akhil-EM <akhilemcalicut@gmail.com>
2021-06-22 20:43:36 +0530

    changes 3

--- server.js
+++ server.js
@@ -1,48 +1,18 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-// // const path = require('path');
-// // const express = require('express');
-// // const app = express();
-// const port = process.env.PORT || 3000;
-// const publicPath = path.join(__dirname, '..', 'public');
-
-
-// app.use(express.static(publicPath));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(publicPath, 'index.html'));
-//  });
-
-// app.get('/',(req,res)=>{
-//    res.json({status:'success'});
-// })
-
-// app.listen(port, () =>{
-//    console.log(port)
-// })
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', 'public');
 
 
-const express=require("express");
-const app=express();
-const port=process.env.PORT ;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+ });
 
-
-
-
-
-
-
-
-//base route to ensure working 
-app.get("/",(req,res)=>{
-    res.status(200).json({Message:"success",Data:"application running"})
-});
-
-
-
-
-
-
-app.listen(port,()=>{
-   console.log(`node js application is running on ${port}`);
-});
+app.listen(port, () =>{
+   console.log(port)
+})
